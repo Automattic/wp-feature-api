19ae15d484931529d52d3095433dcc8b06d55966
Shreve <justin@automattic.com>
2025-04-09 16:44:46 -0400

    Hook up working client examples for navigate and paragraph and heading

--- packages/client/src/types.ts
+++ packages/client/src/types.ts
@@ -14,9 +14,3 @@ export interface Feature {
 export interface FeaturesState {
 	featuresById: Record< string, Feature >;
 }
-
-// Declare global variables provided by WordPress
-// Currently used for the navigate feature, but we may want to handle this a different way
-declare global {
-	const ajaxurl: string | undefined;
-}
