43604cf3019274cb510941cc9ed2b18bbc584bf4
Shreve <justin@automattic.com>
2025-04-10 15:18:16 -0400

    Fix some type errors, and remove redundent REST API definition

--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -1,14 +1,7 @@
 /**
  * WordPress dependencies
  */
-import {
-	createReduxStore,
-	register,
-	dispatch,
-	resolveSelect,
-} from '@wordpress/data';
-import { store as coreStore } from '@wordpress/core-data';
-import { __ } from '@wordpress/i18n';
+import { createReduxStore, register, resolveSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -17,7 +10,7 @@ import reducer from './reducer';
 import * as actions from './actions';
 import * as selectors from './selectors';
 import * as resolvers from './resolvers';
-import { ENTITY_KIND, ENTITY_NAME, STORE_NAME } from './constants';
+import { STORE_NAME } from './constants';
 
 // Create a global variable to track if the store has been registered
 // This ensures we only register the store once across all imports
@@ -49,26 +42,6 @@ if ( ! isStoreRegistered() ) {
 
 		// Initialize the store
 		resolveSelect( STORE_NAME ).getRegisteredFeatures();
-
-		// Register server features
-		// Use type assertion to fix TypeScript error with coreStore
-		const coreStoreDispatch = dispatch( coreStore as any );
-		if ( coreStoreDispatch ) {
-			// @ts-ignore - Ignoring TypeScript errors for addEntities method
-			coreStoreDispatch.addEntities( [
-				{
-					name: ENTITY_NAME,
-					kind: ENTITY_KIND,
-					baseURL: '/wp/v2/features',
-					baseURLParams: { context: 'edit' },
-					plural: 'features',
-					label: __( 'Features' ),
-					transientEdits: {
-						callback: true,
-					},
-				},
-			] );
-		}
 	} catch ( e ) {
 		// If registration fails, it's likely because the store is already registered
 		window.__WP_FEATURE_API_STORE_REGISTERED = true;
